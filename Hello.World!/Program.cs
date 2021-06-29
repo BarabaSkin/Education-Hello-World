4c8b6025cddb4bed9aa6d729c07a6e60d5a3d72d
Николай Караков <kolyan777.k9@gmail.com>
2021-06-30 00:23:11 +0300

    Добавьте файлы проекта.

---
+++ Hello.World!/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Hello.World_
+{
+    class Program
+    {
+        private static string TryWritenValue(string Val)
+        {
+            Val = Console.ReadLine();            
+            return Val;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World! \nWrite'Esc' to stop programm");
+            bool y = true;
+            while (y)
+            {
+                string t= Console.ReadLine();               
+                if (t == "Esc")
+                {
+                    Console.WriteLine("The end of a programm");
+                    y = false;
+                }
+                else if (t == "esc")
+                {
+                    Console.WriteLine("The end of a programm");
+                    y = false;
+                }
+                else
+                {
+                    Console.WriteLine("You did a mistake");
+                    y = true;
+                }
+            }
+
+
+        }
+    }
+}
