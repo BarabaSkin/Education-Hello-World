e622dbd7b2c59e666615cda346039670cfef5057
Николай Караков <kolyan777.k9@gmail.com>
2021-07-05 18:10:29 +0300

    TheEnd

--- Hello.World!/Program.cs
+++ Hello.World!/Program.cs
@@ -4,39 +4,133 @@ namespace Hello.World_
 {
     class Program
     {
-        private static string TryWritenValue(string Val)
-        {
-            Val = Console.ReadLine();            
-            return Val;
-        }
-
-
-        static void Main(string[] args)
-        {
-
-            Console.WriteLine("Hello World! \nWrite'Esc' to stop programm");
-            bool y = true;
-            while (y)
+        
+            private static bool TryGetValue(out int val)
             {
-                string t= Console.ReadLine();               
-                if (t == "Esc")
+                var String = Console.ReadLine();
+                bool isInt = Int32.TryParse(String, out val);
+                return isInt;
+            }
+            private static bool IsLengthZero(int[] source)
+            {
+                if (source.Length == 0)
                 {
-                    Console.WriteLine("The end of a programm");
-                    y = false;
-                }
-                else if (t == "esc")
-                {
-                    Console.WriteLine("The end of a programm");
-                    y = false;
+                    return true;
                 }
                 else
                 {
-                    Console.WriteLine("You did a mistake");
-                    y = true;
+                    return false;
+                }
+            }
+            private static bool EndOrRepeat()
+            {
+                Console.WriteLine("\nДля завершения программы напишите'Exit' \nЧтобы начать программу заново напишите 'Repeat'");
+                bool z = true;
+                bool r = true;
+                while (z)
+                {
+                    var end = Console.ReadLine();
+                    if (r = end == "Exit")
+                    {
+                        Console.WriteLine("Завершение программы");
+                        z = false;
+                        r = false;
+                    }
+                    else if (r = end == "Repeat")
+                    {
+                        Console.WriteLine("Начало программы заново");
+                        z = false;
+                        r = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы ввели неправильное значение! \nПовторите ввод");
+                        z = true;
+                    }
+                }
+                return r;
+            }
+            private static int[] NewArray(int[] source, int value)
+            {
+                var length = source.Length - 1;
+                var result = new int[length];
+                var isSkiped = false;
+                var iterator = 0;
+                foreach (var item in source)
+                {
+                    if (item == value && !isSkiped)
+                    {
+                        isSkiped = true;
+
+                    }
+                    else
+                    {
+                        result[iterator] = item;
+                        iterator++;
+                    }
+                }
+                return result;
+            }
+            static void Main(string[] args)
+            {
+
+                bool endOrNot = true;
+                while (endOrNot)
+                {
+
+                    var random = new Random();
+                    int[] array = new int[3];
+                    for (var C = 0; C < 3; C++)
+                    {
+                        var ii = random.Next(1, 5);
+                        array[C] = ii;
+                    }
+
+                    Console.WriteLine("Дано 3 числа от 1 до 5. Числа могут повторяться! \nУгадайте их!");
+                    bool lengthZero = IsLengthZero(array);
+                    while (!lengthZero)
+                    {
+                        Console.WriteLine("\n Введите предполагаемое число: ");
+                        bool isNumber = true;
+
+                        isNumber = TryGetValue(out int k);
+                        if (isNumber)
+                        {
+                            var guess = true;
+                            while (guess)
+                            {
+                                for (var i = 0; i < array.Length; i++)
+                                {
+                                    if (array[i] == k)
+                                    {
+                                        Console.WriteLine("Вы угадали :)");
+                                        array = NewArray(array, k);
+                                        guess = false;
+                                        break;
+                                    }
+                                }
+                                if (guess == true)
+                                {
+                                    Console.WriteLine("Вы не угадали");
+                                    guess = false;
+                                }
+                            }
+
+                            lengthZero = IsLengthZero(array);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Вы ввели не число!");
+                            isNumber = true;
+                        }
+
+                    }
+                    Console.WriteLine("Вы угадали все числа!");
+                    endOrNot = EndOrRepeat();
                 }
             }
 
-
-        }
+        
     }
 }
+
